aaeeb829a91ad1b3fcbb3167f1ae934b724ccf94
Martin Jaskulla <martin.jaskulla@accenture.com>
2021-03-01 14:11:33 +0100

    Generate detail and overview modules and pages

---
+++ src/app/detail/detail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailPageComponent } from './detail-page/detail-page.component';
+
+
+
+@NgModule({
+  declarations: [DetailPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DetailModule { }
