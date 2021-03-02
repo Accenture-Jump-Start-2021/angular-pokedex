9cec6d68de6152e49a8a6e317238d39c98b92bdf
Martin Jaskulla <martin.jaskulla@accenture.com>
2021-03-02 11:03:50 +0100

    Use rxjs

--- src/app/detail/detail-page/detail-page.component.ts
+++ src/app/detail/detail-page/detail-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { SinglePokemonService } from '../../single-pokemon.service';
+import { Component } from '@angular/core';
+import { PokemonDetail, SinglePokemonService } from '../../single-pokemon.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { filter, pluck } from 'rxjs/operators';
+import { filter, map, pluck, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -9,22 +10,13 @@ import { filter, pluck } from 'rxjs/operators';
   templateUrl: './detail-page.component.html',
   styleUrls: ['./detail-page.component.scss']
 })
-export class DetailPageComponent implements OnInit {
-  src = '';
-  name = '';
+export class DetailPageComponent {
+  id$: Observable<string> = this.activatedRoute.queryParams.pipe(pluck('id'), filter(Boolean)) as Observable<string>;
+  pokemon$: Observable<PokemonDetail> = this.id$.pipe(switchMap(id => this.singlePokemonService.getPokemon(id)));
+  src$: Observable<string> = this.pokemon$.pipe(map(pokemon => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`));
+  name$: Observable<string> = this.pokemon$.pipe(pluck('name'));
 
   constructor(private router: Router, private singlePokemonService: SinglePokemonService, private activatedRoute: ActivatedRoute) {
-  }
-
-  ngOnInit(): void {
-    this.activatedRoute.queryParams.pipe(pluck('id'), filter(Boolean)).subscribe(id => this.search(id as string));
-  }
-
-  search(search: string): void {
-    this.singlePokemonService.getPokemon(search).subscribe(pokemon => {
-      this.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
-      this.name = pokemon.name;
-    });
   }
 
   updateQueryParam(search: string): void {
@@ -37,3 +29,4 @@ export class DetailPageComponent implements OnInit {
       });
   }
 }
+
